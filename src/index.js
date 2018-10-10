bcb2ab44d945e4c5b0f202f28f0e067d31293975
Shenbaga Ganesh <shenbaga.ganesh@tarento.com>
2018-10-10 06:20:36 +0530

    Included treeshaking concept

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
 import Icon from './icon.png';
 import Data from './data.xml'
+import { cube,square } from './math.js'; // ES2015 module syntax
 function component() {
 	let element = document.createElement('div');
 	// Lodash, currently included via a script, is required for this line to work
@@ -13,4 +14,15 @@ function component() {
    	console.log(Data);
     return element;
 }
+//Example for tree shaking
+function treeShaking(){
+    var element = document.createElement('pre');
+    element.innerHTML = [
+        'Hello webpack!',
+        '5 cubed is equal to ' + cube(5),
+        '5 squared is equal to '+ square(5)
+    ].join('\n\n');
+    return element;
+}
 document.body.appendChild(component());
+document.body.appendChild(treeShaking());
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 module.exports = {
+    mode: 'production',
+    watch: false,
     entry: './src/index.js',
     output: {
       	filename: 'bundle.js',
